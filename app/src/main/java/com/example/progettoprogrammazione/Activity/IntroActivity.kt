e4541b51d2a21f9fe19b026183800654900f0672
MattSona99 <matteo.sonaglioni@gmail.com>
2022-11-21 17:52:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/progettoprogrammazione/Activity/IntroActivity.kt
@@ -0,0 +1,28 @@
+package com.example.progettoprogrammazione.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.progettoprogrammazione.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        val loginbtn = findViewById<ConstraintLayout>(R.id.ConstraintLogin)
+        loginbtn.setOnClickListener() {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity (intent)
+        }
+
+        val registerbtn = findViewById<TextView>(R.id.registrati)
+        registerbtn.setOnClickListener() {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/progettoprogrammazione/Activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.example.progettoprogrammazione.Activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.progettoprogrammazione.R
+
+
+class RegisterActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrati)
+    }
+}
