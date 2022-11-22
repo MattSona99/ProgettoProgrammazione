f140ac08178bb6bba3a17c0b8ead2f1ec9dd5cb9
AlessandroPieragostini <S1090384@studenti.univpm.it>
2022-11-22 18:03:33 +0100

    navbar cliccabile che porta ai vari fragment

--- app/src/main/java/com/example/progettoprogrammazione/activity/MainActivity.kt
+++ app/src/main/java/com/example/progettoprogrammazione/activity/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.progettoprogrammazione.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.progettoprogrammazione.R
 import com.example.progettoprogrammazione.databinding.ActivityMainBinding
+import com.example.progettoprogrammazione.fragment.FragmentProfilo
+import com.example.progettoprogrammazione.fragment.FragmentRistoranti
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentProfilo = FragmentProfilo();
+    private val fragmentRistoranti = FragmentRistoranti();
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var user: FirebaseAuth
@@ -18,15 +26,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        replaceFragment(fragmentRistoranti)
+
+        binding.navbar.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_dashboard -> replaceFragment(fragmentRistoranti)
+                R.id.ic_profile -> replaceFragment(fragmentProfilo)
+                R.id.ic_logout -> {
+                    user.signOut()
+                    Toast.makeText(this, "Logout effettuato con successo", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this,IntroActivity :: class.java)
+                    startActivity(intent)
+                }
+            }
+            true
+        }
+
         getResturantData()
         user = FirebaseAuth.getInstance()
-
+/*
         //FUNZIONE CHE SHOWA I RISTORANTI
         val mainActivity = this
         binding.recycleView.apply {
             layoutManager = GridLayoutManager(applicationContext, 2)
             adapter = ResturantAdapter(resturantList)
         }
+ */
+
     }
 
     private fun getResturantData() {
@@ -50,6 +76,13 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+    private fun replaceFragment(fragment : Fragment){
+        if (fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 
 }
