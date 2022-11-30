7ca9fc8a8b1dc0464d3828d847c815715bab5dd7
AlessandroPieragostini <S1090384@studenti.univpm.it>
2022-11-30 12:00:49 +0100

    inizio navigation (non completa) e resturant detail

--- app/src/main/java/com/example/progettoprogrammazione/activity/MainActivity.kt
+++ app/src/main/java/com/example/progettoprogrammazione/activity/MainActivity.kt
@@ -5,24 +5,21 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import com.example.progettoprogrammazione.R
 import com.example.progettoprogrammazione.databinding.ActivityMainBinding
-import com.example.progettoprogrammazione.databinding.FragmentRistorantiBinding
 import com.example.progettoprogrammazione.fragment.FragmentProfilo
 import com.example.progettoprogrammazione.fragment.FragmentRistoranti
 //import com.example.progettoprogrammazione.models.RESTAURANT_EXTRA
 import com.example.progettoprogrammazione.models.Restaurant
 //import com.example.progettoprogrammazione.models.restaurantList
-import com.example.progettoprogrammazione.ristorante.*
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity()
 //    , RestaurantClickListener
 {
 
-    private val fragmentProfilo = FragmentProfilo();
-    private val fragmentRistoranti = FragmentRistoranti();
+    private val fragmentProfilo = FragmentProfilo()
+    private val fragmentRistoranti = FragmentRistoranti()
 
     private lateinit var binding: ActivityMainBinding
 
@@ -41,7 +38,13 @@ class MainActivity : AppCompatActivity()
         binding.navbar.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.ic_dashboard -> replaceFragment(fragmentRistoranti)
-                R.id.ic_profile -> replaceFragment(fragmentProfilo)
+                R.id.ic_profile -> {
+                    replaceFragment(fragmentProfilo)
+/*                    val intent = Intent(this, ProfileActivity::class.java)
+                    startActivity(intent)
+                    
+ */
+                }
                 R.id.ic_logout -> {
                     user.signOut()
                     Toast.makeText(this, "Logout effettuato con successo", Toast.LENGTH_LONG).show()
