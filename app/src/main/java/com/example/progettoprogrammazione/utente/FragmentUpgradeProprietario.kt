91624085aac467865f9ae4a4942c118e95016738
AlessandroPieragostini <S1090384@studenti.univpm.it>
2022-12-21 17:37:41 +0100

    seleziona foto funzionante

--- app/src/main/java/com/example/progettoprogrammazione/utente/FragmentUpgradeProprietario.kt
+++ app/src/main/java/com/example/progettoprogrammazione/utente/FragmentUpgradeProprietario.kt
@@ -1,23 +1,36 @@
 package com.example.progettoprogrammazione.utente
 
+import android.app.Activity
+import android.content.ContentResolver
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.progettoprogrammazione.BuildConfig.APPLICATION_ID
 import com.example.progettoprogrammazione.activity.RestaurateurActivity
 import com.example.progettoprogrammazione.databinding.FragmentUpgrProprietarioBinding
 import com.example.progettoprogrammazione.models.Restaurant
 import com.example.progettoprogrammazione.utils.*
 import com.example.progettoprogrammazione.viewmodels.RestaurantViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.components.BuildConfig.APPLICATION_ID
+import com.google.firebase.database.BuildConfig
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.collection.BuildConfig.APPLICATION_ID
+import java.io.File
 import java.util.*
 
-class FragmentUpgradeProprietario : Fragment(), UserUtil, RestaurantUtils {
+class FragmentUpgradeProprietario : Fragment(), UserUtil, RestaurantUtils, ImgUtils {
 
     private lateinit var binding: FragmentUpgrProprietarioBinding
 
@@ -25,6 +38,13 @@ class FragmentUpgradeProprietario : Fragment(), UserUtil, RestaurantUtils {
     override var firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
 
     private lateinit var restaurantData: Restaurant
+
+    lateinit var ImageUri : Uri
+    private val CHOOSE_PHOTO = 2
+    private val selectImageFromGalleryResult = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        uri?.let { previewImage.setImageURI(uri) }
+    }
+    private val previewImage by lazy { binding.previewImage }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,7 +58,9 @@ class FragmentUpgradeProprietario : Fragment(), UserUtil, RestaurantUtils {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        binding.selectImgNewR.setOnClickListener{
+            selectImageFromGallery()
+        }
         binding.ConstraintCreaRist.setOnClickListener {
 
             binding.nomeristoranteNewR.text.toString()
@@ -49,6 +71,7 @@ class FragmentUpgradeProprietario : Fragment(), UserUtil, RestaurantUtils {
             val telefonoR = binding.telefonoNewR.text.toString()
             val tipoCiboR = binding.tipociboNewR.text.toString()
             val veganR = binding.veganNewR
+            uploadImage()
 
             if (nomeR.length > 20) {
                 binding.nomeristoranteNewR.error =
@@ -115,4 +138,11 @@ class FragmentUpgradeProprietario : Fragment(), UserUtil, RestaurantUtils {
             }
         }
     }
+
+    override fun selectImageFromGallery() = selectImageFromGalleryResult.launch("image/*")
+
+    override fun uploadImage() {
+        TODO("Not yet implemented")
+    }
+
 }
