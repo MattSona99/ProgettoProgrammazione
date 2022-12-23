fa9249a043a610be5ae6bcdada6bac7f31d37f09
MattSona99 <matteo.sonaglioni@gmail.com>
2022-12-23 11:00:07 +0100

    Fast commit carica img

--- app/src/main/java/com/example/progettoprogrammazione/ristoratore/FragmentCreaRist.kt
+++ app/src/main/java/com/example/progettoprogrammazione/ristoratore/FragmentCreaRist.kt
@@ -84,13 +84,9 @@ class FragmentCreaRist : Fragment(), UserUtil, RestaurantUtils, ImgUtils {
                     "Il numero di telefono deve contenere almeno 9 caratteri"
             }
 
-            if (fileName.isEmpty()) {
-                binding.selectImgNewR.error = "Carica un'immagine."
-            }
-
             val vegan: Boolean = veganR.isChecked
 
-            if (nomeR.isNotEmpty() && descrizioneR.isNotEmpty() && indirizzoR.isNotEmpty() && orariolavorativoR.isNotEmpty() && telefonoR.isNotEmpty() && fileName.isNotEmpty()) {
+            if (nomeR.isNotEmpty() && descrizioneR.isNotEmpty() && indirizzoR.isNotEmpty() && orariolavorativoR.isNotEmpty() && telefonoR.isNotEmpty()) {
                 getUserData(object : FireBaseCallbackUser {
                     override fun onResponse(responseU: ResponseUser) {
 
