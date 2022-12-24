368eb1518fc06c3a42765e0acfea36f3fa1b43e0
MattSona99 <matteo.sonaglioni@gmail.com>
2022-12-24 19:24:48 +0100

    RestaurantDetail.kt e SearchBar implementate

--- app/src/main/java/com/example/progettoprogrammazione/ristorante/RestaurantDetail.kt
+++ app/src/main/java/com/example/progettoprogrammazione/ristorante/RestaurantDetail.kt
@@ -1,6 +1,6 @@
 package com.example.progettoprogrammazione.ristorante
 
-import android.graphics.BitmapFactory
+import android.graphics.*
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,11 +14,11 @@ import com.example.progettoprogrammazione.models.Restaurant
 import com.google.firebase.storage.FirebaseStorage
 import java.io.File
 
-class RestaurantDetail : Fragment(){
+class RestaurantDetail : Fragment() {
 
     private lateinit var binding: FragmentRestaurantDetailBinding
 
-    private var restaurantList : ArrayList<Restaurant>? = null
+    private var restaurantList: ArrayList<Restaurant>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,26 +32,28 @@ class RestaurantDetail : Fragment(){
         val restaurantID = args?.get("restID")
         restaurantList = args?.getParcelableArrayList("restArrayList")
 
-        val restaurant=restaurantFromId(restaurantID.toString())
+        val restaurant = restaurantFromId(restaurantID.toString())
 
-        if(restaurant != null)
-        {
+        if (restaurant != null) {
             val imageName = restaurant.imageR
             val storageRef = FirebaseStorage.getInstance().reference.child("$imageName")
-            val localfile = File.createTempFile("tempImage","jpg")
+            val localfile = File.createTempFile("tempImage", "jpg")
             storageRef.getFile(localfile).addOnSuccessListener {
-                val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+                var bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+                bitmap = getCircularBitmap(bitmap)
                 binding.imgRistoranteDetail.setImageBitmap(bitmap)
             }.addOnFailureListener {
-                Toast.makeText(context, "Caricamento immagine fallito",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Caricamento immagine fallito", Toast.LENGTH_SHORT).show()
             }
 
             binding.nomeRistoranteDetail.text = restaurant.nomeR
             binding.descrizioneDetail.text = restaurant.descrizioneR
+            binding.numeroDetail.text = restaurant.telefonoR
+            binding.indirizzodetail.text = restaurant.indirizzoR
 
         }
 
-        binding.visualizzaMenu.setOnClickListener{
+        binding.visualizzaMenu.setOnClickListener {
             view?.findNavController()?.navigate(R.id.DetailToMenu)
         }
 
@@ -60,10 +62,41 @@ class RestaurantDetail : Fragment(){
 
     private fun restaurantFromId(restaurantID: String?): Restaurant? {
         for (restaurant in restaurantList!!) {
-            if(restaurant.idR == restaurantID)
+            if (restaurant.idR == restaurantID)
                 return restaurant
         }
         return null
+    }
+
+    private fun getCircularBitmap(srcBitmap: Bitmap?): Bitmap {
+        val squareBitmapWidth = Integer.min(srcBitmap!!.width, srcBitmap.height)
+        // Initialize a new instance of Bitmap
+        // Initialize a new instance of Bitmap
+        val dstBitmap = Bitmap.createBitmap(
+            squareBitmapWidth,  // Width
+            squareBitmapWidth,  // Height
+            Bitmap.Config.ARGB_8888 // Config
+        )
+        val canvas = Canvas(dstBitmap)
+        // Initialize a new Paint instance
+        // Initialize a new Paint instance
+        val paint = Paint()
+        paint.isAntiAlias = true
+        val rect = Rect(0, 0, squareBitmapWidth, squareBitmapWidth)
+        val rectF = RectF(rect)
+        canvas.drawOval(rectF, paint)
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        // Calculate the left and top of copied bitmap
+        // Calculate the left and top of copied bitmap
+        val left = ((squareBitmapWidth - srcBitmap.width) / 2).toFloat()
+        val top = ((squareBitmapWidth - srcBitmap.height) / 2).toFloat()
+        canvas.drawBitmap(srcBitmap, left, top, paint)
+        // Free the native object associated with this bitmap.
+        // Free the native object associated with this bitmap.
+        srcBitmap.recycle()
+        // Return the circular bitmap
+        // Return the circular bitmap
+        return dstBitmap
     }
 
 }
