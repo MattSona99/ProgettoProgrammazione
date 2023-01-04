dc8dd556827c99710bad2dd0ac532d689073e6ea
MattSona99 <matteo.sonaglioni@gmail.com>
2023-01-04 11:05:50 +0100

    Crea Menù Activity funzionante (2)

--- app/src/main/java/com/example/progettoprogrammazione/activity/CreaMenu.kt
+++ app/src/main/java/com/example/progettoprogrammazione/activity/CreaMenu.kt
@@ -1,10 +1,16 @@
 package com.example.progettoprogrammazione.activity
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.progettoprogrammazione.R
 import com.example.progettoprogrammazione.databinding.FragmentCreaMenuBinding
@@ -25,6 +31,8 @@ class CreaMenu : AppCompatActivity(), ProductUtils, RestaurantUtils {
     override var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
     private lateinit var binding: FragmentCreaMenuBinding
+
+    private var pressedTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -106,5 +114,34 @@ class CreaMenu : AppCompatActivity(), ProductUtils, RestaurantUtils {
         }
         addDialog.create()
         addDialog.show()
+    }
+
+    override fun onBackPressed() {
+        if (pressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed()
+            finishAffinity()
+        } else {
+            Toast.makeText(baseContext, "Premi indietro di nuovo per uscire.", Toast.LENGTH_SHORT)
+                .show()
+        }
+        pressedTime = System.currentTimeMillis()
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+
+        if (ev?.action == MotionEvent.ACTION_UP) {
+            val v: View? = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm: InputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
